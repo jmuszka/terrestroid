e81da7b23b0b10cdbc67561ff9d21a9675dba1c4
Josh Muszka <josh@jmuszka.com>
2025-06-16 20:56:23 -0400

    chore: decoupled and reorganized code

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 
 #include <iostream>
 #include <ostream>
+#include <stdlib.h>
 
 #define SCREEN_W 800
 #define SCREEN_H 600
+
+// Window object
+GLFWwindow* window;
 
 // Triangle
 float vertices[] = {
@@ -18,10 +22,11 @@ float vertices[] = {
 unsigned int VBO;
 unsigned int VAO;
 
-// Link shaders to shader program
+// Shader program and shaders
 unsigned int shaderProgram;
 unsigned int vertexShader;
 unsigned int fragmentShader;
+
 
 // Resize viewport upon resizing window
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -29,13 +34,47 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window)
+// Handle keyboard input
+void processInput()
 {
     // Close window when pressing escape key or 'q'
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) || glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, 1);
 }
 
+// Initialize GLFW and GLAD; create window
+void init()
+{
+    // Configure exact OpenGL specification for GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Required for MacOS
+
+    // Create window object
+    window = glfwCreateWindow(SCREEN_W, SCREEN_H, "Terrestroid", NULL, NULL); // width, height, name, opaque monitor object, opqaue window object
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(-1);
+    }
+    glfwMakeContextCurrent(window); // Make the context of our window the main context on the current thread
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Resize viewport upon resizing window
+
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;;
+        exit(-1);
+    }
+
+    // Set size of viewport to be window size
+    glViewport(0, 0, SCREEN_W, SCREEN_H); // x, y, w, h
+}
+
+// Compile GLSL code
 void compileShaders()
 {
     // Vertex shader
@@ -57,6 +96,7 @@ void compileShaders()
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        exit(-1);
     }
 
 
@@ -77,6 +117,7 @@ void compileShaders()
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        exit(-1);
     }
 
     shaderProgram = glCreateProgram();
@@ -93,34 +134,24 @@ void compileShaders()
     }
 }
 
+// Drawing commands
 void draw()
 {
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Copy user-defined data into currently-bound buffer
-
-    // // Tell OpenGL how to parse vertex data
-    // /* params:
-    //  position of vertex attribute,
-    //  size of vertex attribute,
-    //  type of data,
-    //  whether or not to normalize the data,
-    //  stride (space between consecutive vertex attributes),
-    //  offset
-    // */
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
-    // glEnableVertexAttribArray(0);
-
-    // // Activate shader program object
-    // glUseProgram(shaderProgram);
-
+    // Use the shader program and bind vertex array each time we draw
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
 
+    // Clear the background
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set clear color with specified color (state-setting)
+    glClear(GL_COLOR_BUFFER_BIT); // Clear the background (state-using)
+    
+    // Draw triangle
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     glDrawArrays(GL_TRIANGLES, 0, 3); // OpenGL primitive type, starting index of the vertex array, how many vertices to draw
 }
 
 // Rendering loop
-void render(GLFWwindow *window)
+void render()
 {
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
@@ -133,15 +164,10 @@ void render(GLFWwindow *window)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
 
+    // Rendering loop
     while (!glfwWindowShouldClose(window))
     {
-        // Input
-        processInput(window);
-
-        // Rendering commands
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set clear color with specified color (state-setting)
-        glClear(GL_COLOR_BUFFER_BIT); // Clear the background (state-using)
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        processInput();   
         draw();
 
         // Check and call events and swap the buffers
@@ -155,37 +181,11 @@ void render(GLFWwindow *window)
 }
 
 int main() {
-
-    // Configure exact OpenGL specification for GLFW
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Required for MacOS
-
-    // Create window object
-    GLFWwindow* window = glfwCreateWindow(SCREEN_W, SCREEN_H, "Terrestroid", NULL, NULL); // width, height, name, opaque monitor object, opqaue window object
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window); // Make the context of our window the main context on the current thread
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Resize viewport upon resizing window
-
-    // Initialize GLAD
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;;
-        return -1;
-    }
-
-    // Set size of viewport to be window size
-    glViewport(0, 0, SCREEN_W, SCREEN_H); // x, y, w, h
+    init();
 
     compileShaders();
-    render(window);
+    render();
+
     glfwTerminate();
     return 0;
 }
